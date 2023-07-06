cb84d4eda3ee09d5ab589de188f2be0cbdebfb63
Wonjae Cho <51027177+jadencho1014@users.noreply.github.com>
2023-07-05 22:01:42 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jaden Cho on 2023-07-05.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
